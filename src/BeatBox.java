61328f6f51672fdd59465cd24a7b2440e27da88e
yusuf33NN <m-muradov@list.ru>
2020-10-29 17:59:21 +0300

    full offline MIDIPlayer

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -18,5 +18,175 @@ public class BeatBox {
             "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell",
             "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi-Conga"};
 
+    int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
+
+
+    public static void main(String[] args) {
+        new BeatBox().buildGUI();
+    }
+
+    public void buildGUI() {
+    theFrame = new JFrame("Cyber BeatBox");
+    theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    BorderLayout layout = new BorderLayout();
+    JPanel background = new JPanel(layout);
+    background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+    checkboxList = new ArrayList<>();
+    Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+    JButton start = new JButton("Start");
+    start.addActionListener(new MyStartListener());
+    buttonBox.add(start);
+
+    JButton stop = new JButton("Stop");
+    stop.addActionListener(new MyStopListener());
+    buttonBox.add(stop);
+
+    JButton upTempo = new JButton("Tempo Up");
+    upTempo.addActionListener(new MyUpTempoListener());
+    buttonBox.add(upTempo);
+
+    JButton downTempo = new JButton("Tempo Down");
+    downTempo.addActionListener(new MyDownTempoListener());
+    buttonBox.add(downTempo);
+
+    Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i <16; i++) {
+            nameBox.add(new Label(instrumentNames[i]));
+        }
+
+    background.add(BorderLayout.EAST, buttonBox);
+    background.add(BorderLayout.WEST, nameBox);
+
+    theFrame.getContentPane().add(background);
+
+    GridLayout grid = new GridLayout(16,16);
+    grid.setVgap(1);
+    grid.setHgap(2);
+    mainPanel = new JPanel(grid);
+    background.add(BorderLayout.CENTER, mainPanel);
+
+        for (int i = 0; i < 256; i++) {
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkboxList.add(c);
+            mainPanel.add(c);
+        } // ending of cycle
+    setUpMidi();
+
+    theFrame.setBounds(50,50,300,300);
+    theFrame.pack();
+    theFrame.setVisible(true);
+
+}// close method
+
+    public void setUpMidi() {
+    try {
+        sequencer = MidiSystem.getSequencer();
+        sequencer.open();
+        sequence = new Sequence(Sequence.PPQ,4);
+        track = sequence.createTrack();
+        sequencer.setTempoInBPM(120);
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+} //Closing method setUpMidi
+
+
+    public void buildTrackAndStart() {
+        int[] trackList;
+
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+
+            int key = instruments[i];
+
+            for (int j = 0; j < 16; j++) {
+                JCheckBox jc = checkboxList.get(j + (16 * i));
+
+                if (jc.isSelected()) {
+                    trackList[j] = key;
+                } else {
+                    trackList[j] = 0;
+                }
+            } // Closing inner cycle
+            makeTracks(trackList);
+            track.add(makeEvent(176,1,127,0,16));
+        }// closing out cycle
+
+        track.add(makeEvent(192,9,1,0,15));
+
+        try {
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // closing method buildTrackAndStart
+
+
+
+
+    private class MyStartListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            buildTrackAndStart();
+        }
+    }
+
+
+    private class MyStopListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            sequencer.stop();
+        }
+    }
+
+    private class MyUpTempoListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * 1.03));
+        }
+    }
+
+    private class MyDownTempoListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * 0.97));
+        }
+    }
+
+    public void makeTracks(int[] list) {
+        for (int i = 0; i < 16; i++) {
+            int key = list[i];
+            if (key != 0) {
+                track.add(makeEvent(144,9,key,100,i));
+                track.add(makeEvent(128,9,key,100,i + 1));
+
+            }
+        }
+    } // closing method makeTracks
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd,chan, one,two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return event;
+    }
+
 
 }
